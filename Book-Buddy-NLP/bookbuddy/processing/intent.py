bc14ff7eab5149e136283f0cbffe712b0bd05db6
Farhad Khurami <farhadkhurami@Farhads-MacBook-Pro-2.local>
2024-03-15 19:37:40 +0000

    update

--- Book-Buddy-NLP/bookbuddy/processing/intent.py
+++ Book-Buddy-NLP/bookbuddy/processing/intent.py
@@ -1,1 +1,5 @@
 # This module determines the user's intent (e.g., seeking recommendations) based on the analyzed text.
+import torch
+
+print(torch.__version__)
+print("Is CUDA available:", torch.cuda.is_available())
