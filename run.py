eec8ca4c18c83884c1bd3430bc923184f2453607
Farahad Khurami <fkhurami98@gmail.com>
2024-04-13 16:32:39 +0100

    Update

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from src.intent_recogniser import IntentRecogniser
 from config import SPACY_MODEL_NAME
 from src.utils.utilities import install_spacy_model, load_json_data
 
+example_output_1 = {"genres": set(), "authors": set()}
+example_output_2 = {"genres": {"science"}, "authors": {"isaac asimov"}}
+example_output_3 = {"genres": {"romance"}, "authors": {"jane austen"}}
+
 
 def main():
     install_spacy_model(SPACY_MODEL_NAME)
@@ -10,7 +14,6 @@ def main():
     genres = load_json_data("data/genres.json")
 
     intent_recogniser = IntentRecogniser(genres, SPACY_MODEL_NAME)
-
 
     cli_handler = CLIHandler(intent_recogniser)
     cli_handler.run()
