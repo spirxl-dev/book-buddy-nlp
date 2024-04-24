7c591a17d44c788718bf6155235e42ddfcc54768
Farahad Khurami <fkhurami98@gmail.com>
2024-04-24 12:40:44 +0100

    Changed GoogleBooksAggregator to pass genres at init

--- src/aggregators/google_books_aggregator.py
+++ src/aggregators/google_books_aggregator.py
@@ -6,15 +6,16 @@ from ..utils.utilities import (
 
 
 class GoogleBooksAggregator:
-    def __init__(self, api_key):
+    def __init__(self, api_key, genres_path):
         self.api_key = api_key
         self.base_url = "https://www.googleapis.com/books/v1/volumes"
+        self.genres = load_json_data(genres_path)
 
     def fetch_books(self, query):
         params = {
             "key": self.api_key,
             "q": query,
-            "maxResults": 30,
+            "maxResults": 1,
         }
         response = requests.get(self.base_url, params=params)
         return response.json()
@@ -24,27 +25,25 @@ class GoogleBooksAggregator:
         authors = set()
         for item in items:
             volume_info = item.get("volumeInfo", {})
+            isbn_13 = None
+            for identifier in volume_info.get("industryIdentifiers", []):
+                if identifier["type"] == "ISBN_13":
+                    isbn_13 = identifier["identifier"]
+                    break
+
             book = {
                 "title": volume_info.get("title"),
                 "authors": volume_info.get("authors", []),
                 "publishedDate": volume_info.get("publishedDate"),
-                "isbn": next(
-                    (
-                        identifier["identifier"]
-                        for identifier in volume_info.get("industryIdentifiers", [])
-                        if identifier["type"] == "ISBN_13"
-                    ),
-                    None,
-                ),
+                "isbn": isbn_13,
             }
             books.append(book)
             authors.update(book["authors"])
         return books, authors
 
-    def download_books_by_genre(self, genres_json_path) -> list:
-        genres = load_json_data(genres_json_path)
+    def download_books_by_genre(self) -> list:
         genre_queries = []
-        for genre in genres:
+        for genre in self.genres:
             genre_queries.append("subject:" + genre)
 
         all_books = []
@@ -54,10 +53,9 @@ class GoogleBooksAggregator:
             all_books.extend(books)
         return all_books
 
-    def download_authors_by_genre(self, genres_json_path) -> set:
-        genres = load_json_data(genres_json_path)
+    def download_authors_by_genre(self) -> set:
         genre_queries = []
-        for genre in genres:
+        for genre in self.genres:
             genre_queries.append("subject:" + genre)
 
         all_authors = set()
@@ -67,10 +65,10 @@ class GoogleBooksAggregator:
             all_authors.update(authors)
         return all_authors
 
-    def download_and_save_books(self, genres_path, output_path):
-        books = self.download_books_by_genre(genres_path)
+    def download_and_save_books(self, output_path):
+        books = self.download_books_by_genre()
         save_json_data(books, output_path)
 
-    def download_and_save_authors(self, genres_path, output_path):
-        authors = self.download_authors_by_genre(genres_path)
+    def download_and_save_authors(self, output_path):
+        authors = self.download_authors_by_genre()
         save_json_data(list(authors), output_path)
