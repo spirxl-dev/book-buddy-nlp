9cae8099cff998ea6ffa85875ae220b379adf329
Farahad Khurami <fkhurami98@gmail.com>
2024-04-26 17:40:32 +0100

    added recommendation arg to cli_handler and some other small changes

--- run.py
+++ run.py
@@ -1,49 +1,19 @@
 from src.cli_handler import CLIHandler
 from src.intent_recogniser import IntentRecogniser
 from src.recommendation_engine import RecommendationEngine
-from src.aggregators.google_books_aggregator import GoogleBooksAggregator
-from config import SPACY_MODEL_NAME, GOOGLE_BOOKS_API_KEY, GENRES
-from src.utils.utilities import install_spacy_model, load_json_data
-from pprint import pprint
+from config import SPACY_MODEL_NAME, GENRES
+from src.utils.utilities import install_spacy_model
 
 
-def main_aggregator():
-
-    aggregator = GoogleBooksAggregator(api_key=GOOGLE_BOOKS_API_KEY)
-
-    aggregator.download_and_save_books(output_path="data/books.json")
-
-
-def main_demo_recommendations():
-    engine = RecommendationEngine("data/books.json")
-
-    example_output_empty = {"genres": set(), "authors": set()}
-
-    example_output_2 = {"genres": {"Literary Criticism"}, "authors": {"Lisa Zunshine"}}
-    example_output_3 = {"genres": {"horror"}, "authors": {"stephen king"}}
-
-    pprint(engine.recommend(example_output_3))
-
-
-def main_run_cli():
+def main():
     install_spacy_model(SPACY_MODEL_NAME)
 
-    # What happens if a non valid json is passed?
-    recommender = RecommendationEngine("data/books.json")
-
     intent_recogniser = IntentRecogniser(GENRES, SPACY_MODEL_NAME)
+    recommendation_engine = RecommendationEngine("data/books.json")
 
-    _, _, details = intent_recogniser.process_query(
-        input_string="I want to read a science book"
-    )
-    recommendations = recommender.recommend(details)
-
-    print("\nRecommendations based on your input:")
-    pprint(recommendations)
-
-    # cli_handler = CLIHandler(intent_recogniser)
-    # cli_handler.run()
+    cli_handler = CLIHandler(intent_recogniser, recommendation_engine)
+    cli_handler.run()
 
 
 if __name__ == "__main__":
-    main_run_cli()
+    main()
--- src/cli_handler.py
+++ src/cli_handler.py
@@ -1,6 +1,7 @@
 class CLIHandler:
-    def __init__(self, intent_recogniser):
+    def __init__(self, intent_recogniser, recommendation_engine):
         self.intent_recogniser = intent_recogniser
+        self.recommendation_engine = recommendation_engine
 
     def get_user_input(self):
         return input("\nYour query: ")
@@ -9,6 +10,16 @@ class CLIHandler:
         print(f"\n[RESULT] Identified Named Entities: {named_entities}")
         print(f"[RESULT] Identified Intent: {intent}")
         print(f"[RESULT] Output Dictionary: {details}")
+
+    def display_recommendations(self, recommendations):
+        if recommendations:
+            print("\n[RECOMMENDATIONS]")
+            for book in recommendations:
+                print(
+                    f"Title: {book.get('title')}\nAuthor: {', '.join(book.get('authors', []))}\n"
+                )
+        else:
+            print("\nNo recommendations found based on your query.")
 
     def run(self):
         """ENTRY POINT"""
@@ -20,5 +31,8 @@ class CLIHandler:
                     user_input
                 )
                 self.display_extracted_info(named_entities, intent, details)
+
+                recommendations = self.recommendation_engine.recommend(details)
+                self.display_recommendations(recommendations)
         except KeyboardInterrupt:
             print("\nExiting Book-Buddy...")
