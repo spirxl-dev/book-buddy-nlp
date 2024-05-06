db67e7b76cf56ab136180da2ceceda7eb98a3d72
Farahad Khurami <fkhurami98@gmail.com>
2024-05-06 14:04:17 +0100

    Added intent_recognition for DATE and WORK_OF_ART. also updated relevant tests

--- src/intent_recogniser.py
+++ src/intent_recogniser.py
@@ -40,7 +40,7 @@ class IntentRecogniser:
             entities (list of tuple): Named entities identified from the user's input.
 
         Returns:
-            list of str: A list of identified intents such as 'genre_recommendation', 'author_query', or 'unknown'.
+            list of str: A list of identified intents such as 'genre_recommendation', 'author_query', 'work_of_art_query', 'date_query', or 'unknown'.
         """
         intents = []
         for token in tokens:
@@ -48,11 +48,13 @@ class IntentRecogniser:
                 intents.append("genre_recommendation")
                 break
 
-        if entities:
-            for entity in entities:
-                if entity[1] in {"PERSON", "ORG"}:
-                    intents.append("author_query")
-                    break
+        entity_types = {ent[1] for ent in entities}
+        if "PERSON" in entity_types or "ORG" in entity_types:
+            intents.append("author_query")
+        if "WORK_OF_ART" in entity_types:
+            intents.append("work_of_art_query")
+        if "DATE" in entity_types:
+            intents.append("date_query")
 
         if not intents:
             intents.append("unknown")
@@ -66,7 +68,7 @@ class IntentRecogniser:
         Args:
             tokens (list of str): Preprocessed tokens from the user's input.
             entities (list of tuple): Named entities identified from the user's input.
-            detail_type (str): Type of detail to extract, either 'genres' or 'author'.
+            detail_type (str): Type of detail to extract, either 'genres', 'author', 'work_of_art', or 'date'.
 
         Returns:
             set: Details corresponding to the specified detail type.
@@ -76,9 +78,17 @@ class IntentRecogniser:
             for token in tokens:
                 if token in self.genres:
                     details.add(token)
-        elif detail_type == "author" and entities:
+        elif detail_type == "author":
             for entity in entities:
                 if entity[1] == "PERSON":
+                    details.add(entity[0])
+        elif detail_type == "work_of_art":
+            for entity in entities:
+                if entity[1] == "WORK_OF_ART":
+                    details.add(entity[0])
+        elif detail_type == "date":
+            for entity in entities:
+                if entity[1] == "DATE":
                     details.add(entity[0])
         return details
 
@@ -96,7 +106,12 @@ class IntentRecogniser:
         entities = self.identify_entities(input_string)
         intents = self.get_query_intents(tokens, entities)
 
-        details = {"genres": set(), "authors": set()}
+        details = {
+            "genres": set(),
+            "authors": set(),
+            "works_of_art": set(),
+            "dates": set(),
+        }
         for intent in intents:
             if intent == "genre_recommendation":
                 details["genres"].update(
@@ -105,6 +120,14 @@ class IntentRecogniser:
             elif intent == "author_query":
                 details["authors"].update(
                     self.extract_intent_details(tokens, entities, "author")
+                )
+            elif intent == "work_of_art_query":
+                details["works_of_art"].update(
+                    self.extract_intent_details(tokens, entities, "work_of_art")
+                )
+            elif intent == "date_query":
+                details["dates"].update(
+                    self.extract_intent_details(tokens, entities, "date")
                 )
 
         return entities, intents, details
