6a1fbc2ed9a7c247aa9d55bc5c76b5cfb6fab743
Farahad Khurami <fkhurami98@gmail.com>
2024-07-08 22:16:49 +0100

    update

--- api/controllers/recommend_books_controller.py
+++ api/controllers/recommend_books_controller.py
@@ -5,11 +5,15 @@ from src.services.intent_recogniser import IntentRecogniser
 from src.services.entity_recogniser import EntityRecogniser
 from src.config import SPACY_MODEL_NAME, GENRES
 
-entity_recogniser = EntityRecogniser(GENRES, SPACY_MODEL_NAME)
+
 
 
 def recommend_books_logic(request: QueryRequest) -> BookRecommendationResponse:
+
+    entity_recogniser = EntityRecogniser(GENRES, SPACY_MODEL_NAME)
     entities = entity_recogniser.return_entities(request.input_string)
+
     intents = IntentRecogniser().get_query_intents(entities=entities, genres=GENRES)
-    # book_recommender = BookRecommender(json_file_path="data/books.json")
+    
+    book_recommender = BookRecommender(json_file_path="data/books.json")
     return BookRecommendationResponse(intents=intents, entities=entities)
